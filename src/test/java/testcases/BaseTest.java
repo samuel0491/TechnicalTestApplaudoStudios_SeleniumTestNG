74c2e2d7f067e4c2696bad5dfc22c7a3f2831973
samuel0491 <samuelmedina0491@gmail.com>
2021-10-04 18:23:14 -0400

    Adding multi-browser option to run the test cases

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -1,23 +1,49 @@
 package testcases;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.*;
 import pages.YourLogoHomePage;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    protected ChromeDriver driver;
+    //protected ChromeDriver driver;
+    protected WebDriver driver;
     private final String URL = "http://automationpractice.com/index.php";//TODO: this value could be in a config file
     protected YourLogoHomePage yourLogoHomePage;
 
+    @Parameters({"browser"})
     @BeforeTest
-    public void setUp()throws Exception{
-        //
-        System.setProperty("webdriver.chrome.driver",
-                System.getProperty("user.dir")+"\\src\\test\\java\\drivers\\chromedriver.exe");
+    public void setUp(String browser)throws Exception{
 
-        driver = new ChromeDriver();
+            switch (browser.toLowerCase()){
+
+                case "chrome":
+                    setWebDriverProperty("webdriver.chrome.driver",
+                            "chromedriver.exe");
+                        driver = new ChromeDriver();
+                    break;
+
+                case "firefox":
+                    setWebDriverProperty("webdriver.gecko.driver",
+                            "geckodriver.exe");
+                        driver = new FirefoxDriver();
+                    break;
+
+                case "ie":
+                    setWebDriverProperty("webdriver.ie.driver",
+                            "IEDriverServer.exe");
+                    driver = new InternetExplorerDriver();
+                    break;
+
+                default:
+                    setWebDriverProperty("webdriver.chrome.driver",
+                            "chromedriver.exe");
+                    driver = new ChromeDriver();
+            }
         //
         driver.manage()
                 .window()
@@ -29,6 +55,12 @@ public class BaseTest {
         //
         driver.get(URL);
         //
+    }
+
+    public void setWebDriverProperty(String pathDriver, String driverRunner){
+
+        System.setProperty(pathDriver,
+                System.getProperty("user.dir")+"\\src\\test\\java\\drivers\\"+driverRunner);
     }
 
     @AfterTest
